dec04e93b0cd4b87b82dcb828c993d39f84dac91
bizothesup <Supercreator5>
2020-06-10 14:52:41 +0000

    declaration des variables  methodes lauchHome lauchRegister

--- app/src/main/java/net/mbs/ybma/LoginActivity.kt
+++ app/src/main/java/net/mbs/ybma/LoginActivity.kt
@@ -2,10 +2,73 @@ package net.mbs.ybma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthProvider
+import net.mbs.ybma.commons.PrefManager
+import net.mbs.ybma.utils.CustomDialog
 
 class LoginActivity : AppCompatActivity() {
+    val TAG = "LOGINACTIVITY"
+    private val APP_REQUEST_CODE = 7171 // Any number
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var listener: FirebaseAuth.AuthStateListener
+    private lateinit var dialog: AlertDialog
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var mCallBack: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var mverificationId: String
+    private lateinit var timer: CountDownTimer
+
+    var prefManager: PrefManager? = null
+
+
+    override fun onResume() {
+        super.onResume()
+        prefManager = PrefManager(this)
+        if (!prefManager!!.isFirstTimeLaunch7()) {
+            finish()
+        }
+        CustomDialog.progressDialog(this).dismiss()
+
+    }
+
+
+    fun launchHomeScreen() {
+        CustomDialog.progressDialog(this).dismiss()
+        prefManager!!.setFirstTimeLaunch7(false)
+        //  val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+        intent.putExtra("fragment_name", "")
+        startActivity(intent)
+        finish()
+    }
+
+    fun launchResgisterScreen() {
+        CustomDialog.progressDialog(this).dismiss()
+        // val intent = Intent(this@LoginActivity, InscriptionActivity::class.java)
+        // intent.putExtra("phone", nowithCode)
+        startActivity(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        prefManager = PrefManager(this)
+        if (!prefManager!!.isFirstTimeLaunch7()) {
+            launchHomeScreen();
+        }
+        initView()
+        firebaseAuth = FirebaseAuth.getInstance()
+        phoneNumberAuthCallbackListener()
+    }
+
+    private fun phoneNumberAuthCallbackListener() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initView() {
+        TODO("Not yet implemented")
     }
 }
